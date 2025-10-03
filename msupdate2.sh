b6bb279e6f070ad0ab15c957ba6aeaf30f3ce0c9
Ford DAI <31793442+eiheiheiOwO@users.noreply.github.com>
2025-10-03 10:48:59 +0800

    Update and rename msupdate1.sh to msupdate2.sh

--- msupdate2.sh
+++ msupdate2.sh
@@ -21,7 +21,7 @@ echo -e "${GREEN}Auto-Update script for miaospeed & frpc with cron setup.${NC}"
 
 # --- 配置 ---
 proxy_prefix="https://gh.685763.xyz/"
-ghapi="api.github.com" # Removed protocol for clarity
+ghapi="gh.685763.xyz/api.github.com" # Removed protocol for clarity
 MIAOKO_DIR="/miaoko"
 FRPC_FILE="$MIAOKO_DIR/frpc"
 MIAOSPEED_FILE_PATTERN="$MIAOKO_DIR/miaospeed-linux-"
